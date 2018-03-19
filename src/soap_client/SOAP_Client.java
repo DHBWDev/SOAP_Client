bae49dd1dc96123f012473dcaadb8e609cd29c33
vm <vm@vm-VirtualBox>
2018-03-19 18:46:42 +0100

    Merge origin/master
    Conflicts:
    	.gitignore

--- src/soap_client/SOAP_Client.java
+++ src/soap_client/SOAP_Client.java
@@ -5,12 +5,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
 import javax.xml.ws.Holder;
 
 
 public class SOAP_Client {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, DatatypeConfigurationException {
         
         
         
@@ -80,14 +85,25 @@ public class SOAP_Client {
     }
 
     public static void kundeAnlegen() throws IOException {
+        
+        WebService_Service wsService = new WebService_Service();
+        WebService ws = wsService.getWebServicePort();
 
-        String vorname = readString("Vorname:");
-        String nachname = readString("Nachname:");
-        String strasse = readString("Straße:");
-        int hausnummer = readInt("Hausnummer:");
-        int plz = readInt("Postleitzahl:");
-        String ort = readString("Ort:");
-        String land = readString("Land");
+        Customer newCustomer = new Customer();
+        newCustomer.setFirstName(readString("Vorname: "));
+        newCustomer.setName(readString("Nachname: "));
+        newCustomer.setStreet(readString("Straße: "));
+        newCustomer.setHouseNumber(readString("Hausnummer: "));
+        newCustomer.setPostCode(readString("Postleitzahl: "));
+        newCustomer.setPlace(readString("Ort: "));
+        newCustomer.setCountry(readString("Land: "));
+        
+        Holder<Customer> customer = new Holder<Customer>(newCustomer);
+        
+        ws.saveNewCustomer(customer);
+        
+        System.out.println("Sie haben die Kundennummer: " + customer.value.getId());
+        
 
     }
 
@@ -111,11 +127,11 @@ public class SOAP_Client {
 
     }
 
-    public static void fahrzeugAusleihen() throws IOException {
+    public static void fahrzeugAusleihen() throws IOException, DatatypeConfigurationException{
         
         WebService_Service wsService = new WebService_Service();
         WebService ws = wsService.getWebServicePort();
-        List<Car> cars = ws.findAll();
+        List<Car> cars = ws.findAllCars();
         
         System.out.println("Folgende Fahrzeuge stehen zur Verfügung:");
         
@@ -131,13 +147,31 @@ public class SOAP_Client {
         }
         
 
-        int kundennummer = readInt("Kundennummer:");
+        long kundennummer = readLong("Kundennummer:");
+        long carId = readLong("FahrzeugID:");
         String von = readString("Abholdatum (yyyy-mm-dd)");
         String bis = readString("Rückgabedatum (yyyy-mm-dd)");
+        
+        DatatypeFactory dtf = DatatypeFactory.newInstance();
+        XMLGregorianCalendar startTimeFrom = dtf.newXMLGregorianCalendar(von);
+        XMLGregorianCalendar endeTimeFrom = dtf.newXMLGregorianCalendar(bis);
+        
+        try {
+            ws.saveNewContract(startTimeFrom, endeTimeFrom, kundennummer, carId);
+        } catch (CarIsNotAvailableException_Exception ex) {
+            
+            System.out.println("Fahrzeug ist nicht verfügbar");
+        }
 
     }
 
     public static void leihverträgeAuflisten() throws IOException {
+        
+        WebService_Service wsService = new WebService_Service();
+        WebService ws = wsService.getWebServicePort();
+        List<Contract> contracts = ws.findContractsByCustomerId(readLong("Kundennummer:"));
+        
+        System.out.println("Folgende Fahrzeuge stehen zur Verfügung:");
         
         
         
@@ -156,6 +190,30 @@ public class SOAP_Client {
         return input;
     }
 
+    public static long readLong(String anzeige) throws IOException {
+
+        Boolean again = true;
+        long inint = 0;
+
+        while (again) {
+            again = false;
+            System.out.print(anzeige);
+            BufferedReader fromKeyboard = new BufferedReader(new InputStreamReader(System.in));
+            String input;
+
+            input = fromKeyboard.readLine();
+
+            try {
+                inint = Long.parseLong(input);
+            } catch (NumberFormatException e) {
+                again = true;
+                System.out.println("Hoppla Sie haben sich wahrscheinlich vertippt... Probieren Sie's nochmal...");
+            }
+        }
+
+        return inint;
+    }
+    
     public static int readInt(String anzeige) throws IOException {
 
         Boolean again = true;
